536de5517fa9fd06c980e7118366b686642292a2
Foxies777 <gaazovalmaz589@gmail.com>
2023-11-17 19:38:35 +0300

    push-notification

--- src/Notification.jsx
+++ src/Notification.jsx
@@ -1,8 +1,8 @@
-import addNotification from 'react-push-notification';
+import addNotification, { Notifications } from 'react-push-notification';
 
 function Notification(props) {
     if (props.message !== undefined) {
-        let message = props.message.split(' ')
+        let message = props.message.split(', ')[0]
         console.log(message);
         addNotification({
             title: 'Вы находитесь на',
@@ -12,7 +12,11 @@ function Notification(props) {
         // Здесь возвращайте null (или другой компонент), если вы не хотите ничего рендерить
         return null;
     }
-
+    return (
+        <div className="Notification">
+            <Notifications />
+        </div>
+    )
 }
 
 export default Notification;
