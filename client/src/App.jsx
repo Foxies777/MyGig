a4d0645982bc32e5958536a5f74b69d3dff8f0ed
Foxies777 <gaazovalmaz589@gmail.com>
2023-12-02 16:07:19 +0300

    large commit added server

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+import Map from './components/Map'
+
+function App() {
+
+  return (
+    <>
+      <Map/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ client/src/components/Notification.jsx
@@ -0,0 +1,15 @@
+import addNotification from 'react-push-notification';
+
+function Notification(props) {
+    if (props.message !== undefined) {
+        let message = props.message.split(', ')[0];
+        console.log(message);
+        addNotification({
+            title: 'Вы находитесь на',
+            message: message,
+            native: true
+        });
+    }
+}
+
+export default Notification;
