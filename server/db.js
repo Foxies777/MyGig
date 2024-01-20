b41424bcd07ae2f4286eb62d50d46c9663b4af6d
Foxies777 <gaazovalmaz589@gmail.com>
2024-01-20 17:51:19 +0300

    db.js update

--- server/db.js
+++ server/db.js
@@ -1,12 +1,17 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
+const pg = require('pg');
+
+pg.defaults.ssl = true;
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
-)
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialectModule: pg, // Явно указываем драйвер
+    ssl: true, // Включаем SSL, если это необходимо
+  }
+);
